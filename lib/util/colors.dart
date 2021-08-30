70445053e706f78a16f5e190fc1280fb9985af6f
black-redoc <joseb.twelve@gmail.com>
2021-08-30 17:23:03 -0500

    feature: added purple_primary and purple_secondary to util/colors.dart

---
+++ lib/util/colors.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart' show Color;
+
+class LocalColors {
+  static const Color purple_primary = Color(0xFF9528D4);
+  static const Color purple_secondary = Color(0xFFC62EF0);
+}
