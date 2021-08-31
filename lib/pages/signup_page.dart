351f55ef290f183591713662f04befa1de5cae80
black-redoc <joseb.twelve@gmail.com>
2021-08-31 12:57:16 -0500

    feature: added SignupPage

---
+++ lib/pages/signup_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class SignupPage extends StatefulWidget {
+  SignupPage({Key? key}) : super(key: key);
+
+  @override
+  _SignupPageState createState() => _SignupPageState();
+}
+
+class _SignupPageState extends State<SignupPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        color: Colors.red
+      )
+    );
+  }
+}
--- lib/routes.dart
+++ lib/routes.dart
@@ -1,10 +1,12 @@
 import 'package:crud_products/pages/login_page.dart';
+import 'package:crud_products/pages/signup_page.dart';
 import 'package:flutter/material.dart' show BuildContext, WidgetBuilder;
 
 
 class LocalRoutes {
   static final Map<String, WidgetBuilder> _routes = {
-    "/login": (BuildContext context) => LoginPage()
+    "/login": (BuildContext context) => LoginPage(),
+    "/signup": (BuildContext context) => SignupPage()
   };
 
   static Map<String, WidgetBuilder> get routes => _routes;
