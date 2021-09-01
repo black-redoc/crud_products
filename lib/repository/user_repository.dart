c0af5d3a0728bcb2f36e61f63bf899f2285e7b3c
black-redoc <joseb.twelve@gmail.com>
2021-09-01 08:26:43 -0500

    feature: added user_repository with firebase auth

---
+++ lib/repository/user_repository.dart
@@ -0,0 +1,41 @@
+import 'dart:async';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+class UserRepository {
+  final FirebaseAuth _firebaseAuth;
+
+  UserRepository({
+    FirebaseAuth? firebase,
+    GoogleSignIn? googleSignIn
+  }): _firebaseAuth = firebase ?? FirebaseAuth.instance;
+
+  Future<UserCredential> signInWithCredentials(String email, String password) async {
+    return await _firebaseAuth.signInWithEmailAndPassword(
+      email: email, 
+      password: password
+    );
+  }
+
+  Future<UserCredential> signUpWithCredentials(String email, String password) async {
+    return await _firebaseAuth.createUserWithEmailAndPassword(
+      email: email,
+      password: password
+    );
+  }
+
+  Future<void> signOut() async {
+    await Future.wait([
+      _firebaseAuth.signOut(),
+    ]);
+  }
+
+  Future<bool> isSignedIn() async {
+    final currentUser = _firebaseAuth.currentUser;
+    return currentUser != null;
+  }
+
+  Future<String> getUser() async {
+    return _firebaseAuth.currentUser!.email!;
+  }
+}
