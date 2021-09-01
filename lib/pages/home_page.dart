4071660d45819d87d3b5394e96ae189d5a6aaa9c
black-redoc <joseb.twelve@gmail.com>
2021-09-01 11:19:46 -0500

    feature: added title bar to home page

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:crud_products/util/colors.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -10,13 +11,42 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
     return Scaffold(
-      bottomNavigationBar: Container(),
-      appBar: AppBar(
-        title: Center(
-          child: Text("Productos")
-        )
-      ),
+      body: Container(
+        height: size.height,
+        child: Column(
+          children: <Widget>[
+            _titleBar(height: size.height, width: size.width)
+          ]
+        ),
+      )
     );
   }
+
+  Widget _titleBar({ double? height, double? width }) => Container(
+    height: height! * 0.23,
+    width: width,
+    color: LocalColors.purple_dark,
+    child: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.only(left: 30),
+              child: Text(
+                "Productos",
+                style: TextStyle(
+                  fontSize: 30,
+                  color: Colors.white70
+                )
+              ),
+            )
+          ],
+        )
+      ]
+    )
+  );
 }
