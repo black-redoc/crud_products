20a2f69bbbd9e399c59efb2b8dbdffbca5bb2c5f
black-redoc <joseb.twelve@gmail.com>
2021-09-01 14:47:44 -0500

    feature: added product lists to home page

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:crud_products/util/colors.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -17,7 +18,10 @@ class _HomePageState extends State<HomePage> {
         height: size.height,
         child: Column(
           children: <Widget>[
-            _titleBar(height: size.height, width: size.width)
+            _titleBar(height: size.height, width: size.width),
+            Expanded(
+              child: _productList(width: size.width)
+            ),
           ]
         ),
       )
@@ -49,4 +53,140 @@ class _HomePageState extends State<HomePage> {
       ]
     )
   );
+
+  Widget _productList({ double? width }) {
+    return ListView.builder(
+      padding: EdgeInsets.zero,
+      shrinkWrap: true,
+      itemCount: 10,
+      itemBuilder: (context, idx) {
+        return Container(
+          width: width,
+          decoration: BoxDecoration(
+            border: Border(
+              top: BorderSide(color: Colors.grey, width: 0.5),
+              bottom: (idx == (List.generate(10, (i) => i).length - 1))
+                ? BorderSide(color: Colors.grey, width: 0.5)
+                : BorderSide.none,
+            )
+          ),
+          child: Dismissible(
+            key: Key("$idx"),
+            confirmDismiss: (DismissDirection direction) =>
+                _confirmDismiss(idx),
+            child: ListTile(
+              title: Center(
+                child:
+                  Text(
+                    "$idx",
+                    style: TextStyle(
+                      color: Colors.red
+                    )
+                  ),
+              ),
+            ),
+            background: _slideRightBackground(),
+            secondaryBackground: _slideLeftBackground(),
+          ),
+        );
+      }
+    );
+  }
+
+  Widget _slideRightBackground() => Container(
+    color: Colors.red,
+    child: Align(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: <Widget>[
+          SizedBox(
+            width: 20,
+          ),
+          Icon(
+            Icons.delete,
+            color: Colors.white,
+          ),
+          Text(
+            " Eliminar",
+            style: TextStyle(
+              color: Colors.white,
+              fontWeight: FontWeight.w700,
+            ),
+            textAlign: TextAlign.left,
+          ),
+        ],
+      ),
+      alignment: Alignment.centerLeft,
+    ),
+  );
+
+  Widget _slideLeftBackground() => Container(
+    color: LocalColors.purple_primary,
+    child: Align(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: <Widget>[
+          Text(
+            " Editar",
+            style: TextStyle(
+              color: Colors.white,
+              fontWeight: FontWeight.w700,
+            ),
+            textAlign: TextAlign.left,
+          ),
+          Icon(
+            Icons.edit,
+            color: Colors.white,
+          ),
+          SizedBox(
+            width: 20,
+          ),
+        ],
+      ),
+      alignment: Alignment.centerRight,
+    ),
+  );
+
+  Future<bool> _confirmDismiss(int id) async {
+    final response = await showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: <Widget>[
+              SizedBox(width: 20),
+              Text("Confirmación"),
+              IconButton(
+                onPressed: () => _backPage(context),
+                icon: Icon(Icons.clear_rounded, size: 20)
+              )
+            ],
+          ),
+          content: Text("Desea eliminar este item?"),
+          actions: <Widget>[
+            MaterialButton(
+              color: Colors.red,
+              child: Text(
+                "Eliminar",
+                style: TextStyle(
+                  color: Colors.white
+                )
+              ),
+              onPressed: () => _deleteInterest(id)
+            ),
+          ],
+        );
+      }
+    );
+    return response;
+  }
+
+  void _backPage(BuildContext context) {
+    Navigator.of(context).pop();
+  }
+
+  void _deleteInterest(id) {
+
+  }
 }
