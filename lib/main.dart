6fa83dd6ad2c9df41836916b74b6297967f2efe9
black-redoc <joseb.twelve@gmail.com>
2021-09-01 11:01:01 -0500

    hotfix: added Firebase.initializeApp() to manage Firebase producs and ensure Firebase is correctly initialized

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,50 @@
 import 'package:crud_products/routes.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
- 
+
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
   runApp(MyApp());
 }
- 
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    return FutureBuilder(
+      future: Firebase.initializeApp(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.done) {
+          return MaterialApp(
+            title: 'CRUD Products',
+            debugShowCheckedModeBanner: false,
+            routes: LocalRoutes.routes,
+            initialRoute: '/login',
+          );
+        }
+        return Loading();
+      }
+    );
+  }
+}
+
+class Loading extends StatefulWidget {
+  Loading({Key? key}) : super(key: key);
+
+  @override
+  _LoadingState createState() => _LoadingState();
+}
+
+class _LoadingState extends State<Loading> {
+  @override
+  Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'CRUD Products',
+      title: 'Loading',
       debugShowCheckedModeBanner: false,
-      routes: LocalRoutes.routes,
-      initialRoute: '/login',
+      home: Scaffold(
+        body: Center(
+          child: CircularProgressIndicator()
+        ),
+      ),
     );
   }
 }
