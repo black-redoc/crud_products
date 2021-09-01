ec66854d91a1af1318938dae2dee08d4681b3690
black-redoc <joseb.twelve@gmail.com>
2021-09-01 11:01:32 -0500

    feature: added home page

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,3 +1,4 @@
+import 'package:crud_products/pages/home_page.dart';
 import 'package:crud_products/pages/login_page.dart';
 import 'package:crud_products/pages/signup_page.dart';
 import 'package:flutter/material.dart' show BuildContext, WidgetBuilder;
@@ -6,7 +7,8 @@ import 'package:flutter/material.dart' show BuildContext, WidgetBuilder;
 class LocalRoutes {
   static final Map<String, WidgetBuilder> _routes = {
     "/login": (BuildContext context) => LoginPage(),
-    "/signup": (BuildContext context) => SignupPage()
+    "/signup": (BuildContext context) => SignupPage(),
+    "/": (BuildContext context) => HomePage()
   };
 
   static Map<String, WidgetBuilder> get routes => _routes;
