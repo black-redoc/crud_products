65860307a0643f615cb7804fa675fff43a9d4a10
black-redoc <joseb.twelve@gmail.com>
2021-09-01 11:19:28 -0500

    feature: added purple_dark color

--- lib/util/colors.dart
+++ lib/util/colors.dart
@@ -3,4 +3,5 @@ import 'package:flutter/material.dart' show Color;
 class LocalColors {
   static const Color purple_primary = Color(0xFF9528D4);
   static const Color purple_secondary = Color(0xFFC62EF0);
+  static const Color purple_dark = Color(0xFF211A2E);
 }
