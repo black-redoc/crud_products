c378cfc1a0e69edded7aa1ec8e93f03c50839c6c
black-redoc <joseb.twelve@gmail.com>
2021-09-02 10:43:49 -0500

    feature: added title to product form page

--- lib/pages/product_form_page.dart
+++ lib/pages/product_form_page.dart
@@ -1,4 +1,5 @@
 import 'package:crud_products/data/product.dart';
+import 'package:crud_products/util/colors.dart';
 import 'package:flutter/material.dart';
 
 class ProductFormPage extends StatelessWidget {
@@ -12,12 +13,56 @@ class ProductFormPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
     return Scaffold(
-      body: Center(
-        child: Text(
-          "${isUpdate ? 'Actualizar' : 'Crear'} Producto"
+      body: Container(
+        height: size.height,
+        child: Column(
+          children: <Widget>[
+            _titleBar(
+              height: size.height,
+              width: size.width,
+              context: context
+            ),
+          ]
         )
       ),
     );
   }
+
+  Widget _titleBar({ 
+    double? height,
+    double? width,
+    BuildContext? context 
+  }) => Container(
+    height: height! * 0.23,
+    width: width,
+    color: LocalColors.purple_dark,
+    child: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            _backButton(context!),
+            Padding(
+              padding: const EdgeInsets.only(left: 30),
+              child: Text(
+                "${isUpdate ? 'Actualizar' : 'Crear'} producto",
+                style: TextStyle(
+                  fontSize: 30,
+                  color: Colors.white70
+                )
+              ),
+            )
+          ],
+        )
+      ]
+    )
+  );
+
+  Widget _backButton(BuildContext context) => IconButton(
+    icon: Icon(Icons.arrow_back_rounded, color: Colors.white),
+    onPressed: () => Navigator.of(context).pop()
+  );
 }
